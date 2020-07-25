fc007c1d919df1e509a3c370c2cc6f92ed55df61
Douglas Adams <douglas.adams@me.com>
2020-07-24 22:26:26 -0400

    Initial Commit

---
+++ TestButtons/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TestButtons
+//
+//  Created by Douglas Adams on 7/24/20.
+//  Copyright © 2020 Douglas Adams. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
